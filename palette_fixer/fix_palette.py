ab02b2088a7402273a1b2c7b073ebeceaad6795d
Buscher <beataroundthebuscher@protonmail.com>
2022-01-23 10:59:45 +0100

    Added Logging Module; Enabled Mode for argv == 2

--- palette_fixer/fix_palette.py
+++ palette_fixer/fix_palette.py
@@ -4,13 +4,22 @@
 # read/write with library - https://stackoverflow.com/questions/64132945/with-pypng-how-do-i-read-a-png-with-it
 # about **metadata - https://www.geeksforgeeks.org/python-star-or-asterisk-operator/
 
+# python asterisk operator - https://www.geeksforgeeks.org/python-star-or-asterisk-operator
+# - Passing a  Function Using with an arbitrary number of positional argument
+# type and keys of metadata
+# <class 'dict'>
+# dict_keys(['greyscale', 'alpha', 'planes', 'bitdepth', 'interlace', 'size', 'physical', 'palette'])
+
 
 import png, re
 import sys
+import logging, datetime
 
 import oxcePalettes
 
 def fixPalette(filePath):
+    logging.debug("Running filePath: " + filePath)
+
     filePath = re.sub("\\n", "", filePath)
     try:
         pngReader = png.Reader(filename=filePath)
@@ -23,15 +32,18 @@ def fixPalette(filePath):
                 break
             found += 1
 
-        print("Wrong Transparent Index found at: " + str(found))
+        logging.info("Wrong Transparent Index found at: " + str(found))
 
         for x in range(0, len(pixels)):
             if pixels[x] == found:
                 pixels[x] = 0
+        
 
+        logging.debug("Removing 'physical' key from 'metadata'")
         if "physical" in metadata.keys():
             metadata.pop("physical")
 
+        logging.debug("TODO: Determine palette by parsing the read palette")
         # some kind of checker to determine which palette the rest uses
         metadata["palette"] = oxcePalettes.battlePalette
 
@@ -43,37 +55,58 @@ def fixPalette(filePath):
         return True
 
     except FileNotFoundError:
-        print("Failed for " + filePath)
+        logging.warning("Failed with FileNotFoundError " + filePath)
         return False
-    except TypeError:
-        print("Failed for " + filePath)
+    except TypeError as e:
+        logging.warning("Failed with TypeError " + filePath)
+        logging.warning(e)
+        raise
         return False
     except png.FormatError:
-        print("Failed for " + filePath)
+        logging.warning("Failed with png.FormatError " + filePath)
         return False
 
+def populatePathsWithOpenXcomLogFile(logFilePath):
+    populatedPathsList = []
 
+    log = open(logFilePath, "r")
+    loglines = log.readlines()
+    log.close()
+
+    for line in loglines: 
+        if "has incorrect transparent color index" in line:
+            line = re.sub("^.*WARN\]\\sImage\\s", "", line)
+            line = re.sub("\\s\(.*$", "", line)
+            populatedPathsList.append(sys.argv[2] + "/" + line)
+
+    return populatedPathsList
+
+# grabbed from https://github.com/EttyKitty/OpenXcom_Tools/commit/19ea1661c8f20f24af9d7ed7ecf9afa6d166f622
+#LOG_FILENAME = ('crashlog.txt')
+LOG_FILENAME = "./logs/" + (datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S.log'), 'a')[0]
+logging.basicConfig(filename=LOG_FILENAME, level=logging.INFO, filemode='w')
+
+
+
+if len(sys.argv) < 2:
+    print("Usage: fix_palette.py path-to-openxcom.log path-to-mod-root-dictionary")
+    print("OR")
+    print("Usage: fix_palette.py path-to-image.png")
+    sys.exit(0)
 
 if len(sys.argv) < 3:
-        print("Usage: fix_palette.py path-to-openxcom.log path-to-mod-root-dictionary")
-        sys.exit(0)
+    fixPalette(sys.argv[1])
+else:
+    filePaths = populatePathsWithOpenXcomLogFile(sys.argv[1])
 
-log = open(sys.argv[1], "r")
-loglines = log.readlines()
+    successCounter = 0
 
-filePaths = []
+    for x in filePaths:
+        if fixPalette(x):
+            successCounter += 1
 
-for line in loglines:
-    if "has incorrect transparent color index" in line:
-        line = re.sub("^.*WARN\]\\sImage\\s", "", line)
-        line = re.sub("\\s\(.*$", "", line)
-        filePaths.append(sys.argv[2] + "/" + line)
+    logging.warning("Found " + str(len(filePaths)) + "broken files by parsing OpenXcom Log")
+    logging.warning("Solved " + str(successCounter) + "broken files ")
 
-for x in filePaths:
-    print(x)
-    fixPalette(x)
-
-
-
-sys.exit(0) # return how many were changed
+    sys.exit(successCounter) # return how many were changed
 
