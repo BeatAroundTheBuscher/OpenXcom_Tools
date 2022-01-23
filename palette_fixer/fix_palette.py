7266f824cd48ae768d6ef31e541132640c156067
Buscher <beataroundthebuscher@protonmail.com>
2022-01-23 22:34:20 +0100

    Implemented guessPalette

--- palette_fixer/fix_palette.py
+++ palette_fixer/fix_palette.py
@@ -16,23 +16,38 @@ import logging, datetime
 
 import oxcePalettes
 
-def guessPalette(palette):
+def guessPalette(metadata):
     # confidence is determined by checking 16 of 256 colors and correctNumber/16
     # the picked colors are the diagonal from NE to SW - should be good enough for approximation
 
-    if len(palette) != 256:
+    if len(metadata["palette"]) != 256:
         logging.error("Image does not have a palette with 256 colors")
         raise ValueError("Image does not have a palette with 256 colors")
         return None
 
-    confidence = 0 
-    if True:
-        logging.info("Guessed battlePaletteUFO")
-        return oxcePalettes.battlePaletteUFO
-    else:
-        logging.info("Failed to guess palette")
-        raise ValueError("No known palette")
-        return None
+    testOrder = [oxcePalettes.battleScapeUFO, oxcePalettes.ufopaediaUFO, oxcePalettes.baseScapeUFO, oxcePalettes.geoScapeUFO]
+    testOrderNames = ["battleScapeUFO", "ufopaediaUFO", "baseScapeUFO", "geoScapeUFO"]
+
+    currentTest = 0
+
+    for test in testOrder:
+        confidence = 0 
+        for i in [15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180, 195, 210, 225, 240, 255]:
+            logging.debug("test value: " + str(test[i]))
+            logging.debug("palette value: " + str(metadata["palette"][i]))
+            if test[i][0:3] == metadata["palette"][i][0:3]:
+                confidence += 1
+        logging.debug("\nConfidence: " + str(confidence))
+
+        if confidence > 13: # of 17
+            logging.info("Guessed Palette: " + testOrderNames[currentTest])
+            return test
+        
+        currentTest += 1
+
+    logging.info("Failed to guess palette")
+    raise ValueError("No known palette")
+    return None
 
 def fixPalette(filePath):
     logging.info("Running filePath: " + filePath)
@@ -42,45 +57,42 @@ def fixPalette(filePath):
         pngReader = png.Reader(filename=filePath)
         w, h, pixels, metadata = pngReader.read_flat()
     
-        # some kind of checker to determine which palette the rest uses
-        logging.debug("TODO: Determine palette by parsing the read palette")
-        guessedPalette = guessPalette(metadata["palette"])
+        guessedPalette = guessPalette(metadata) # 1CH.png
 
         found = 0 # needs a cleaner way
 
-        for x in pngReader.trns:
-            if x == 0:
-                break
-            found += 1
+        if pngReader.trns != None: # todo: Scan for what the log actually says
+            for x in pngReader.trns:
+                if x == 0:
+                    break
+                found += 1
 
-        logging.info("Wrong Transparent Index found at: " + str(found))
-        if found == 0:
-            logging.warning("Wrong Transparent Index SHOULD NOT BE 0")
+            logging.info("Wrong Transparent Index found at: " + str(found))
+            if found == 0:
+                logging.warning("Wrong Transparent Index SHOULD NOT BE 0")
 
-        for x in range(0, len(pixels)): # change picture to set palette index 0 for background
-            if pixels[x] == found:
-                pixels[x] = 0
+        if found != 0:
+            for x in range(0, len(pixels)): # change picture to set palette index 0 for background
+                if pixels[x] == found:
+                    pixels[x] = 0
         
 
         logging.debug("Removing 'physical' key from 'metadata'")
         if "physical" in metadata.keys():
             metadata.pop("physical")
 
-
-
         if guessedPalette is not None:
             metadata["palette"] = guessedPalette
 
-
-            output = open(filePath, 'wb')
+            output = open(filePath, 'wb') # save result
             writer = png.Writer(w, h, **metadata)
             writer.write_array(output, pixels)
             output.close()
             return True
         else:
             return False
-
-    except FileNotFoundError:
+    
+    except FileNotFoundError: # may hide other errors
         logging.warning("Failed with FileNotFoundError " + filePath)
         return False
     except TypeError as e:
@@ -93,6 +105,7 @@ def fixPalette(filePath):
     except ValueError: # contains custom raises
         logging.warning("Failed with ValueError " + filePath)
         return False
+    
 
 def populatePathsWithOpenXcomLogFile(logFilePath):
     populatedPathsList = []
@@ -111,8 +124,7 @@ def populatePathsWithOpenXcomLogFile(logFilePath):
 
 # grabbed from https://github.com/EttyKitty/OpenXcom_Tools/commit/19ea1661c8f20f24af9d7ed7ecf9afa6d166f622
 LOG_FILENAME = "./logs/" + (datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S.log'), 'a')[0]
-logging.basicConfig(filename=LOG_FILENAME, level=logging.INFO, filemode='w')
-
+logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG, filemode='w')
 
 
 if len(sys.argv) < 2:
