4c84aff22d15454e15b71cb14a13137b676c1abc
Buscher <beataroundthebuscher@protonmail.com>
2023-12-28 20:25:58 +0100

    Finding top lines

--- autocutter/autocutter.py
+++ autocutter/autocutter.py
@@ -18,11 +18,8 @@ if len(sys.argv) < 2:
 
 filePath = sys.argv[1]
 
-"""
-tilePieces = []
-tileWidth = 32
-tileHeight = 40
 
+"""
 f = open(filePath, 'rb')
 tilePieces = pngH.xcom_crop(f, tileWidth, tileHeight, tilePieces)
 img = pngH.recreateSpritesheet(f, tileWidth, tileHeight, tilePieces, 8)
@@ -33,74 +30,91 @@ sys.exit(0)
 """
 
 
-def findBottomLeft(img):
+def findTopLeft(img):
     imgWidth, imgHeight = img.size
-
     startPoint = (0, 0)
     endPoint = (0, 0)
 
-    # 2 steps right, 1 step down
+    for startY in range(0, imgHeight - 1):
+        for currentY in range(0, startY):
+            currentX = (startY - currentY) * 2
+            if img.getpixel((currentX, currentY)) != 0 or\
+               img.getpixel((currentX + 1, currentY)) != 0:
+                return startPoint, endPoint
+
+        pixelsToBottom, pixelsToRight = pixelSanityCheck(img, startY)
+        startPoint = (0, startY)
+        # TODO: is that X sane?
+        endPoint = (1 + (imgHeight - pixelsToBottom) * 2, 0)
+    return startPoint, endPoint
+
+
+def findTopRight(img):
+    imgWidth, imgHeight = img.size
+    startPoint = (0, 0)
+    endPoint = (0, 0)
+
+    for startY in range(0, imgHeight - 1):
+        for currentY in range(0, startY):
+            currentX = currentX = imgWidth - 1 - (currentY - startY) * (-2)
+            if img.getpixel((currentX, currentY)) != 0 or\
+               img.getpixel((currentX - 1, currentY)) != 0:
+                return startPoint, endPoint
+
+        pixelsToBottom, pixelsToLeft = pixelSanityCheck(img, startY)
+        startPoint = (imgWidth, startY)
+        # TODO: is that X sane?
+        endPoint = (pixelsToBottom, 0)
+    return startPoint, endPoint
+
+
+def findBottomLeft(img):
+    imgWidth, imgHeight = img.size
+    startPoint = (0, 0)
+    endPoint = (0, 0)
+
     for startY in range(imgHeight - 1, 0, -1):
-        drawBool = True
-        # x can be at most pixelToBottom * 2
-        # canvas (0, 0) is top left
-
-        # y = y0 * -0.5 * x
-        # x = (y0 - y) * -2
-        # y0: startY    y: currentY
-
         for currentY in range(startY, imgHeight - 1):
             currentX = (startY - currentY) * (-2)
             if img.getpixel((currentX, currentY)) != 0 or\
                img.getpixel((currentX + 1, currentY)) != 0:
-                drawBool = False
                 return startPoint, endPoint
 
-        if drawBool:
-            # drawing the lines between two points
-            pixelsToBottom = imgHeight - 1 - startY
-            pixelsToRight = pixelsToBottom * 2
-            if pixelsToRight > imgWidth:
-                pixelsToRight = imgWidth
-                pixelsToBottom = imgWidth / 2
-            startPoint = (0, startY)
-            endPoint = (1 + pixelsToRight, startY + pixelsToBottom)
-            # drawMulticolorLine(img, startPoint, endPoint)
+        pixelsToBottom, pixelsToRight = pixelSanityCheck(img, startY)
+        startPoint = (0, startY)
+        endPoint = (1 + pixelsToRight, startY + pixelsToBottom)
     return startPoint, endPoint
 
 
-def findBottomRight(inputPNG):
+# TODO: Figure out how to fix that one line too few
+def findBottomRight(img):
     imgWidth, imgHeight = img.size
-
     startPoint = (0, 0)
     endPoint = (0, 0)
 
-    # 2 steps left, 1 step down
     for startY in range(imgHeight - 1, 0, -1):
-        drawBool = True
-
-        # y = y0 * -0.5 * (x_max - x)
-        # x = x_max - (y0 - y) * 2
-        # y0: startY    y: currentY
-
         for currentY in range(startY, imgHeight - 1):
             currentX = imgWidth - 1 - (currentY - startY) * 2
             if img.getpixel((currentX, currentY)) != 0 or\
                img.getpixel((currentX - 1, currentY)) != 0:
-                drawBool = False
                 return startPoint, endPoint
 
-        if drawBool:
-            # drawing the lines between two points
-            pixelsToBottom = imgHeight - 1 - startY
-            pixelsToLeft = pixelsToBottom * 2
-            if pixelsToLeft > imgWidth:
-                pixelsToLeft = imgWidth
-                pixelsToBottom = imgWidth / 2
-            startPoint = (imgWidth, startY)
-            endPoint = (imgWidth - pixelsToLeft, startY + pixelsToBottom)
-            # drawMulticolorLine(img, startPoint, endPoint)
+        pixelsToBottom, pixelsToLeft = pixelSanityCheck(img, startY)
+        startPoint = (imgWidth, startY)
+        endPoint = (imgWidth - pixelsToLeft, startY + pixelsToBottom)
     return startPoint, endPoint
+
+
+def pixelSanityCheck(img, startY):
+    imgWidth, imgHeight = img.size
+    # pixelsToTop = startY
+    pixelsToBottom = imgHeight - 1 - startY
+    pixelToSide = pixelsToBottom * 2
+    if pixelToSide > imgWidth:
+        pixelToSide = imgWidth
+        pixelsToBottom = imgWidth / 2
+
+    return pixelsToBottom, pixelToSide
 
 
 def drawMulticolorLine(img, startPoint, endPoint):
@@ -113,6 +127,14 @@ def drawMulticolorLine(img, startPoint, endPoint):
 
     draw = ImageDraw.Draw(img)
     draw.line([startPoint, endPoint], fill=lineColor, width=1)
+
+
+def showSingle(img, tLeftCoord1, tLeftCoord2, tRightCoord1, tRightCoord2,
+               bLeftCoord1, bLeftCoord2, bRightCoord1, bRightCoord2):
+    drawMulticolorLine(img, tLeftCoord1, tLeftCoord2)
+    drawMulticolorLine(img, tRightCoord1, tRightCoord2)
+    drawMulticolorLine(img, bLeftCoord1, bLeftCoord2)
+    drawMulticolorLine(img, bRightCoord1, bRightCoord2)
 
 
 def showGrid(img, bLeftCoord1, bLeftCoord2, bRightCoord1, bRightCoord2):
@@ -134,15 +156,33 @@ def showGrid(img, bLeftCoord1, bLeftCoord2, bRightCoord1, bRightCoord2):
 
 f = open(filePath, 'rb')
 img = Image.open(f)
+tLeftCoord1, tLeftCoord2 = findTopLeft(img)
+tRightCoord1, tRightCoord2 = findTopRight(img)
 bLeftCoord1, bLeftCoord2 = findBottomLeft(img)
 bRightCoord1, bRightCoord2 = findBottomRight(img)
-bLeftCoord1 = (0, 72)
-bLeftCoord2 = (0, 72)
-bRightCoord1 = (104, 72)
-bRightCoord2 = (104, 72)
-showGrid(img, bLeftCoord1, bLeftCoord2, bRightCoord1, bRightCoord2)
+# bLeftCoord1 = (0, 72)
+# bLeftCoord2 = (0, 72)
+# bRightCoord1 = (104, 72)
+# bRightCoord2 = (104, 72)
+showSingle(img, tLeftCoord1, tLeftCoord2,
+           tRightCoord1, tRightCoord2,
+           bLeftCoord1, bLeftCoord2,
+           bRightCoord1, bRightCoord2)
+# showGrid(img, bLeftCoord1, bLeftCoord2, bRightCoord1, bRightCoord2)
 f.close()
-
 img.save("line_image.png")
-# img.show()
 
+
+"""
+tilePieces = []
+tileWidth = 32
+tileHeight = 40
+
+f = open(filePath, 'rb')
+tilePieces = pngH.xcom_crop(f, tileWidth, tileHeight, tilePieces)
+img = pngH.recreateSpritesheet(f, tileWidth, tileHeight, tilePieces, 8)
+img.save("merged_" + sys.argv[1])
+"""
+
+
+# img.show()
